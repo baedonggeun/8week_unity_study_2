b63f995668ac1cf3fd7cb6a216de77ced1c4266b
baedonggeun <hispanie3@gmail.com>
2024-01-18 19:19:42 +0900

    region 주석 처리 캐릭터 방향 설정 함수 및 패키지 추가
    1. 캐릭터 이동 추가 패키지를 통해 구현 위해 이전 이동 코드 region화 및 주석 처리
    2. 이동 방향 또는 캐릭터 기준 마우스 방향에 따라 캐릭터의 몸체 방향 바뀌게 하도록 함수 생성
    3. input manager 패키지 추가

--- Assets/Scripts/Controllers/TopDownCharacterConstoller.cs
+++ Assets/Scripts/Controllers/TopDownCharacterConstoller.cs
@@ -1,25 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TopDownCharacterConstoller : MonoBehaviour
 {
+    public event Action<Vector2> OnMoveEvent;           //event -> 외부에서 호출하지 못하게 막는 기능
+    public event Action<Vector2> OnLookEvent;
+
+    public void CallMoveEvent(Vector2 direction)
+    {
+        OnMoveEvent?.Invoke(direction);         //?. -> ?.앞의 내용이 Null이 아닐 경우만 뒤의 내용 실행
+
+    }
+
+    public void CallLookEvent(Vector2 direction)
+    {
+        OnLookEvent?.Invoke(direction);
+    }
+
+
+
+    #region 이동 처리 예시
     //float speed = 5f;         //유니티 inspector 에서 접근하진 못하지만 코드에 적용되는 변수
     //[SerializeField]private float speed = 5f;         //유니티와 강제적으로 동기화하여 값을 바꿀 수 없게 만든 변수
-    public float speed = 5f;            //유니티 inspector 에서 접근하여 변경 가능한 변수
+    //public float speed = 5f;            //유니티 inspector 에서 접근하여 변경 가능한 변수
+    
 
     // Start is called before the first frame update
-    void Start()
-    {
+    //void Start()
+    //{
         
-    }
+    //}
 
     // Update is called once per frame
-    void Update()
-    {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+    //void Update()
+    //{
 
-        transform.position += new Vector3(x, y) * speed * Time.deltaTime;
-    }
+    //    float x = Input.GetAxis("Horizontal");
+    //    float y = Input.GetAxis("Vertical");
+
+    //    transform.position += new Vector3(x, y) * speed * Time.deltaTime;
+
+    //}
+    #endregion
 }
